abb4a1033fc127923be3ab7fb4fc5b24d37365ac
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-10-17 11:06:13 +0200

    I-13: Creating a minimal Camunda process engine.

---
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/MinimalCryptoExchangeProcessApplication.java
@@ -0,0 +1,11 @@
+package com.dpisarenko.minimalcryptoexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalCryptoExchangeProcessApplication {
+    public static void main(String... args) {
+        SpringApplication.run(MinimalCryptoExchangeProcessApplication.class, args);
+    }
+}
