e4ce297d2f0ab89cf922aca83ff5ff63b24de790
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-10-17 12:10:08 +0200

    I-13: GetEthBalance seems to work now.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/GetEthBalance.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/GetEthBalance.java
@@ -9,6 +9,8 @@ import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.http.HttpService;
 
+import java.math.BigInteger;
+
 public class GetEthBalance implements JavaDelegate {
     private final Logger logger;
 
@@ -21,12 +23,13 @@ public class GetEthBalance implements JavaDelegate {
     }
 
     @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
+    public void execute(DelegateExecution delEx) throws Exception {
         final Web3j web3 = Web3j.build(new HttpService("http://localhost:8178"));
 
-        final EthGetBalance balance = web3.ethGetBalance("0x190FD61ED8fE0067f0f09EA992C1BF96209bab66", DefaultBlockParameterName.LATEST).sendAsync().get();
-
-        logger.debug("Hello!");
-
+        final EthGetBalance response = web3.ethGetBalance("0x190FD61ED8fE0067f0f09EA992C1BF96209bab66", DefaultBlockParameterName.LATEST).sendAsync().get();
+        final BigInteger balanceWei = response.getBalance();
+        logger.debug("Balance in wei: " + balanceWei);
+        delEx.setVariable("EXCHANGE_ACCOUNT_BALANCE_WEI",
+                balanceWei);
     }
 }
