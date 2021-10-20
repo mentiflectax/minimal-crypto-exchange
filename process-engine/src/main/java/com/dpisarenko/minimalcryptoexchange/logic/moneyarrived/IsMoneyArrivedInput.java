c86e18f01ab00a048abda84a3d0c966ac8033fc8
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-10-20 22:19:27 +0200

    I-16: Implementing IsMoneyArrived.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/moneyarrived/IsMoneyArrivedInput.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/moneyarrived/IsMoneyArrivedInput.java
@@ -1,16 +1,38 @@
 package com.dpisarenko.minimalcryptoexchange.logic.moneyarrived;
 
 import com.dpisarenko.minimalcryptoexchange.common.Currency;
+import org.bitcoinj.core.NetworkParameters;
+import org.slf4j.Logger;
 
 public class IsMoneyArrivedInput {
     private Currency currency;
+    private NetworkParameters networkParameters;
+    private Logger logger;
 
     public IsMoneyArrivedInput withCurrency(final Currency newCurrency) {
         currency = newCurrency;
         return this;
     }
 
+    public IsMoneyArrivedInput withnetworkParameters(final NetworkParameters newNetworkParameters) {
+        networkParameters = newNetworkParameters;
+        return this;
+    }
+
+    public IsMoneyArrivedInput withLogger(final Logger newLogger) {
+        logger = newLogger;
+        return this;
+    }
+
     public Currency getCurrency() {
         return currency;
+    }
+
+    public NetworkParameters getNetworkParameters() {
+        return networkParameters;
+    }
+
+    public Logger getLogger() {
+        return logger;
     }
 }
