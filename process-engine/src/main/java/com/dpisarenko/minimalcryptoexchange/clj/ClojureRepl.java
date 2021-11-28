c947d7c9ee145365c641604131c3836ca81cb461
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-11-28 11:47:59 +0100

    I-16: Moved the Clojure file to a different location.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/clj/ClojureRepl.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/clj/ClojureRepl.java
@@ -2,7 +2,6 @@ package com.dpisarenko.minimalcryptoexchange.clj;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
-import com.dpisarenko.minimalcryptoexchange.delegates.GetEthBalance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
