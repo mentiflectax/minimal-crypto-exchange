0e31020452d18f86b2be01757593015ed1eb8d2a
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-09 17:19:21 +0100

    I-16: Tinkering with reception of BTC. Trying to call a Clojure delegate.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/ClojureDelegate.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/ClojureDelegate.java
@@ -3,15 +3,13 @@ package com.dpisarenko.minimalcryptoexchange.delegates;
 import com.dpisarenko.minimalcryptoexchange.clj.ClojureService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("clj-del")
 public class ClojureDelegate implements JavaDelegate {
-    private final ClojureService backend;
-
-    public ClojureDelegate(final ClojureService backend) {
-        this.backend = backend;
-    }
+    @Autowired
+    private ClojureService backend;
 
     @Override
     public void execute(DelegateExecution delEx) throws Exception {
