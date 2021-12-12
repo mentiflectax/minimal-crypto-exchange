af5a997888e7a7cf20c53009b478158ac2a6970a
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-12 10:58:31 +0100

    I-16: Working on BTC-USDT exchange process. Refactoring Clojure functions.

--- process-engine/src/main/resources/com/dpisarenko/core.clj
+++ process-engine/src/main/resources/com/dpisarenko/core.clj
@@ -21,78 +21,6 @@
     (log-info "Clojure subsystem started.")
     ))
 
-;; Various functions (start)
-(declare create-btc-tx)
-(declare append-tx)
-(declare btcTxReceived)
-(declare tx-present?)
-;; Various functions (end)
-
-;; Delegates (start)
-(defn check-btc-arrived
-  [de]
-  (let [
-        tx-id (.getVariable de "INCOMING_TX_ID")
-        btc-arrived (tx-present?
-                      @state
-                      tx-id
-                      "BTC")
-        ]
-    (.setVariable de "BTC_ARRIVED" btc-arrived)
-    (log-info (str "check-btc-arrived: check-btc-arrived: INCOMING_TX_ID: "
-                   tx-id ", BTC arrived: " btc-arrived))
-    ))
-
-(defn set_retry_counter_to_0
-  [de]
-  (.setVariable de "RETRY_COUNTER" 0)
-  )
-
-(defn increment_wait_counter
-  [de]
-  (let [
-        old-value (.getVariable de "RETRY_COUNTER")
-        new-value (+ 1 old-value)
-        ]
-    (.setVariable de "RETRY_COUNTER" new-value)
-    )
-  )
-
-(defn check_max_number_of_wait_cycles_exceeded
-  [de]
-  (let [
-        retries (.getVariable de "RETRY_COUNTER")
-        max-retries 5
-        max-retries-exceeded (> retries max-retries)
-        ]
-    (.setVariable de
-                  "MAX_NUMBER_OF_WAITING_CYCLES_EXCEEDED"
-                  max-retries-exceeded)
-    )
-  )
-;; Delegates (end)
-
-;; Low-level functions (start)
-(defn create-btc-tx
-  [tx-id amount]
-  (let []
-    {
-     :currency   "BTC"
-     :created-at (now)
-     :tx-id      tx-id
-     :amount     amount
-     }
-    ))
-
-(defn append-tx
-  [old-state new-tx]
-  (let [
-        old-tx-list (get old-state :txs)
-        new-tx-list (conj old-tx-list new-tx)
-        ]
-    (assoc old-state :txs new-tx-list)
-    ))
-
 (defn btcTxReceived
   [tx-id amount]
   (let [
@@ -105,23 +33,3 @@
     (log-info (str "New state: " @state))
     )
   )
-
-(defn tx-present?
-  [state tx-id currency]
-  (let [txs (:txs state)
-        matching-txs (filter (fn [cur-tx]
-                               (let [cur-tx-currency (:currency cur-tx)
-                                     cur-tx-id (:tx-id cur-tx)
-
-                                     ]
-                                 (and
-                                   (= currency cur-tx-currency)
-                                   (= tx-id cur-tx-id)))
-                               )
-                             txs
-                             )
-        tx-exists (not (empty? matching-txs))
-        ]
-    tx-exists)
-  )
-;; Low-level functions (end)
--- process-engine/src/main/resources/com/dpisarenko/detect_btc_reception_process.clj
+++ process-engine/src/main/resources/com/dpisarenko/detect_btc_reception_process.clj
@@ -1,1 +1,95 @@
-(ns com.dpisarenko.detect-btc-reception-process)
+(ns com.dpisarenko.detect-btc-reception-process
+  (:require [com.dpisarenko.common :refer :all])
+  )
+
+;; Various functions (start)
+(declare create-btc-tx)
+(declare append-tx)
+(declare btcTxReceived)
+(declare tx-present?)
+;; Various functions (end)
+
+;; Delegates (start)
+(defn check-btc-arrived
+  [de]
+  (let [
+        tx-id (.getVariable de "INCOMING_TX_ID")
+        btc-arrived (tx-present?
+                      @state
+                      tx-id
+                      "BTC")
+        ]
+    (.setVariable de "BTC_ARRIVED" btc-arrived)
+    (log-info (str "check-btc-arrived: check-btc-arrived: INCOMING_TX_ID: "
+                   tx-id ", BTC arrived: " btc-arrived))
+    ))
+
+(defn set_retry_counter_to_0
+  [de]
+  (.setVariable de "RETRY_COUNTER" 0)
+  )
+
+(defn increment_wait_counter
+  [de]
+  (let [
+        old-value (.getVariable de "RETRY_COUNTER")
+        new-value (+ 1 old-value)
+        ]
+    (.setVariable de "RETRY_COUNTER" new-value)
+    )
+  )
+
+(defn check_max_number_of_wait_cycles_exceeded
+  [de]
+  (let [
+        retries (.getVariable de "RETRY_COUNTER")
+        max-retries 5
+        max-retries-exceeded (> retries max-retries)
+        ]
+    (.setVariable de
+                  "MAX_NUMBER_OF_WAITING_CYCLES_EXCEEDED"
+                  max-retries-exceeded)
+    )
+  )
+;; Delegates (end)
+
+;; Low-level functions (start)
+(defn- create-btc-tx
+  [tx-id amount]
+  (let []
+    {
+     :currency   "BTC"
+     :created-at (now)
+     :tx-id      tx-id
+     :amount     amount
+     }
+    ))
+
+(defn- append-tx
+  [old-state new-tx]
+  (let [
+        old-tx-list (get old-state :txs)
+        new-tx-list (conj old-tx-list new-tx)
+        ]
+    (assoc old-state :txs new-tx-list)
+    ))
+
+(defn- tx-present?
+  [state tx-id currency]
+  (let [txs (:txs state)
+        matching-txs (filter (fn [cur-tx]
+                               (let [cur-tx-currency (:currency cur-tx)
+                                     cur-tx-id (:tx-id cur-tx)
+
+                                     ]
+                                 (and
+                                   (= currency cur-tx-currency)
+                                   (= tx-id cur-tx-id)))
+                               )
+                             txs
+                             )
+        tx-exists (not (empty? matching-txs))
+        ]
+    tx-exists)
+  )
+;; Low-level functions (end)
