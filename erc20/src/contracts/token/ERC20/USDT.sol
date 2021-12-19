a5f63e7b7a0ca58b69406118a034f26698013e91
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-19 09:22:33 +0100

    I-16: Implementing USDT contract.

--- erc20/src/contracts/token/ERC20/USDT.sol
+++ erc20/src/contracts/token/ERC20/USDT.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.8.0;
 
 contract ERC20FixedSupply is ERC20 {
-    constructor() public {
-        totalSupply += 1000;
-        balances[msg.sender] += 1000;
+    constructor(uint256 initialSupply) public ERC20("Test Tether", "tUSDT") {
+    	_mint(msg.sender, initialSupply);
     }
 }
