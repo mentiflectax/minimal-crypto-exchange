95c039b8a5966a76e6c33b9704b5df2311795d5d
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-29 08:27:16 +0100

    I-16: Implementing BTC->USD conversion. Trying to send USDT to the exchange account.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/MinimalCryptoExchangeProcessApplication.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/MinimalCryptoExchangeProcessApplication.java
@@ -1,7 +1,11 @@
 package com.dpisarenko.minimalcryptoexchange;
 
+import com.dpisarenko.minimalcryptoexchange.delegates.TransferUsdtToExchangeAccount;
 import com.dpisarenko.minimalcryptoexchange.logic.btc.WalletObserver;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,13 +14,10 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @EnableProcessApplication
 public class MinimalCryptoExchangeProcessApplication {
+    @Autowired
+    private ProcessEngine processEngine;
+
     public static void main(String... args) {
         SpringApplication.run(MinimalCryptoExchangeProcessApplication.class, args);
-    }
-
-    @PostConstruct
-    void startWalletObserver() {
-        // final WalletObserver walletObserver = new WalletObserver();
-        // walletObserver.init();
     }
 }
--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/usdt/TestGasProvider.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/usdt/TestGasProvider.java
@@ -6,8 +6,8 @@ import org.web3j.tx.gas.StaticGasProvider;
 import java.math.BigInteger;
 
 public class TestGasProvider extends StaticGasProvider {
-    public static final BigInteger GAS_PRICE = BigInteger.valueOf(10L);
-    public static final BigInteger GAS_LIMIT = BigInteger.valueOf(1L);
+    public static final BigInteger GAS_PRICE = BigInteger.valueOf(1L);
+    public static final BigInteger GAS_LIMIT = BigInteger.valueOf(1000000000L);
 
     public TestGasProvider() {
         super(GAS_PRICE, GAS_LIMIT);
