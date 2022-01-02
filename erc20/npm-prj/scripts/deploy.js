7ad4963112ffbdb835fc5577d17aace68eed8f2e
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-02 12:52:44 +0100

    I-16: Attempts to set the initial USDT balance of the exchange account.

--- erc20/npm-prj/scripts/deploy.js
+++ erc20/npm-prj/scripts/deploy.js
@@ -1,7 +1,9 @@
 async function main() {
   // We get the contract to deploy
-  const USDT = await ethers.getContractFactory("USDT");
-  const usdt = await USDT.deploy(1000000000000000);
+    const USDT = await ethers.getContractFactory("USDT");
+    // TODO: Add here the exchange address which should
+    // have all the tUSDT
+    const usdt = await USDT.deploy("0x190FD61ED8fE0067f0f09EA992C1BF96209bab66", 1000000000000000);
 
   console.log("USDT contract deployed to:", usdt.address);
 }
--- erc20/src/contracts/token/ERC20/USDT.sol
+++ erc20/src/contracts/token/ERC20/USDT.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "./tokens/ERC20/ERC20.sol";
+import "./ERC20.sol";
 
 contract USDT is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
-    	_mint(msg.sender, initialSupply);
+    constructor(address owner, uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
+    	_mint(owner, initialSupply);
     }
 }
