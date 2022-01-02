c63e7d9b5ec1b6d34d434f4d9afcc8171b61e423
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-02 12:56:55 +0100

    I-16: Attempt at setting the USDT balance of the exchange during deployment of the USDT smart contract.

--- erc20/npm-prj/contracts/USDT.sol
+++ erc20/npm-prj/contracts/USDT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "./tokens/ERC20/ERC20.sol";
 
 contract USDT is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
-    	_mint(msg.sender, initialSupply);
+    constructor(address owner, uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
+    	_mint(owner, initialSupply);
     }
 }
