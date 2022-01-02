56d27895e3a4658fef38ab76b17bd06414357756
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-02 13:16:52 +0100

    I-16: Removed unnecessary files.

--- trash/compile-usdt.sh
+++
@@ -1,6 +0,0 @@
-docker run -v /Users/dp118m/dev/misc/minimal-crypto-exchange/erc20:/sources \
-       ethereum/solc:stable \
-       -o /sources/target \
-       --abi \
-       --bin \
-       /sources/src/contracts/token/ERC20/USDT.sol
--- trash/src/contracts/token/ERC20/USDT.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "./ERC20.sol";
-
-contract USDT is ERC20 {
-    constructor(address owner, uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
-    	_mint(owner, initialSupply);
-    }
-}
