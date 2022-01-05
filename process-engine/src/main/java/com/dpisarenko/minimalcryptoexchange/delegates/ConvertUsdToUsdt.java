f7f93674028bc27b743635617e57cb87fb85444f
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-05 12:05:04 +0100

    I-16: Implementing BTC->USD conversion process. Debugging sending of ETH.

---
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/ConvertUsdToUsdt.java
@@ -0,0 +1,18 @@
+package com.dpisarenko.minimalcryptoexchange.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+
+@Component("ConvertUsdToUsdt")
+public class ConvertUsdToUsdt implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        final Double usdAmount = (Double) delegateExecution.getVariable("USD_AMOUNT");
+        final long usdtAmountLong = (long) (usdAmount * 100000000L);
+        final BigInteger usdtAmount = BigInteger.valueOf(usdtAmountLong);
+        delegateExecution.setVariable("USDT_AMOUNT", usdtAmount);
+    }
+}
