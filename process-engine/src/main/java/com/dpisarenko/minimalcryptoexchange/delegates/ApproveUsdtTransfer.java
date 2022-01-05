af1acef5813990bf0dd7696555917cf68dbb042a
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-05 11:50:04 +0100

    I-16: Implementing BTC->USD conversion process. Debugging sending of ETH.

---
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/ApproveUsdtTransfer.java
@@ -0,0 +1,47 @@
+package com.dpisarenko.minimalcryptoexchange.delegates;
+
+import com.dpisarenko.minimalcryptoexchange.logic.usdt.TestGasProvider;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.web3j.contracts.eip20.generated.ERC20;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+
+import java.math.BigInteger;
+
+@Component("ApproveUsdtTransfer")
+public class ApproveUsdtTransfer implements JavaDelegate {
+    @Value("${accounts.eth.exchange.private-key}")
+    String privateKey;
+
+    @Value("${accounts.eth.usdt.contract-address}")
+    String usdtContractAddress;
+
+    @Value("${networks.eth.url}")
+    String ethNetworkUrl;
+
+    @Value("${accounts.eth.exchange.address}")
+    String exchangeAddress;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        final Web3j web3 = createWeb3If(ethNetworkUrl);
+        final Credentials credentials = Credentials.create(privateKey);
+        final BigInteger gasPrice = web3.ethGasPrice().send().getGasPrice();
+        final ERC20 usdtContract = ERC20.load(usdtContractAddress, web3, credentials, new TestGasProvider(BigInteger.valueOf(1), BigInteger.valueOf(2*Short.MAX_VALUE)));
+
+        final Double usdAmount = (Double) delegateExecution.getVariable("USD_AMOUNT");
+        final long usdAmountLong = (long) (usdAmount * 100L);
+        final BigInteger usdtAmount = BigInteger.valueOf(usdAmountLong);
+        final BigInteger amountToApprove = usdtAmount.add(BigInteger.ONE);
+        usdtContract.approve(exchangeAddress, amountToApprove).send();
+    }
+
+    Web3j createWeb3If(final String url) {
+        return Web3j.build(new HttpService(url));
+    }
+}
