6bcb3fde1d1be532d464061cf45ecacf5e4bdd59
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-08 13:18:56 +0100

    I-16: Polishing the code.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/btc/WalletObserver.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/btc/WalletObserver.java
@@ -2,16 +2,16 @@ package com.dpisarenko.minimalcryptoexchange.logic.btc;
 
 import com.dpisarenko.minimalcryptoexchange.clj.ClojureService;
 import org.bitcoinj.core.Address;
-import org.bitcoinj.core.Coin;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.utils.BriefLogFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
-import java.util.Optional;
 
 @Component
 public class WalletObserver {
@@ -20,6 +20,16 @@ public class WalletObserver {
 
     @Value("${accounts.btc.exchange.address}")
     String exchangeAddress;
+
+    private final Logger logger;
+
+    WalletObserver(Logger logger) {
+        this.logger = logger;
+    }
+
+    public WalletObserver() {
+        this(LoggerFactory.getLogger(WalletObserver.class));
+    }
 
     @PostConstruct
     public void init() {
@@ -32,15 +42,13 @@ public class WalletObserver {
         kit.startAsync();
         kit.awaitRunning();
         try {
-            kit.wallet().addWatchedAddress(Address.fromString(netParams,  exchangeAddress));
-            // TODO: Put the listener into a separate class
+            kit.wallet().addWatchedAddress(Address.fromString(netParams, exchangeAddress));
             // TODO: Write an automated test for the listener
             kit.wallet().addCoinsReceivedEventListener(new BtcReceivedListener(clojureService, exchangeAddress, netParams));
         }
-        catch (Exception exception) {
+        catch (final Exception exception) {
             // TODO: Test this
-            // TODO: Log this error
-            exception.printStackTrace();
+            logger.error("An error occurred while setting up BTC wallet listener", exception);
         }
     }
 }
