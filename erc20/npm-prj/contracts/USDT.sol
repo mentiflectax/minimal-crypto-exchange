8fee1984ede170ea83c62f5c63965d759c84fc45
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-09 16:59:01 +0100

    I-16: Testing the modified (covered with tests) version of the code.

--- erc20/npm-prj/contracts/USDT.sol
+++ erc20/npm-prj/contracts/USDT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "./tokens/ERC20/ERC20.sol";
 
 contract USDT is ERC20 {
-    constructor(address owner, uint256 initialSupply) ERC20("Test Tether", "tUSDT") {
-    	_mint(owner, initialSupply);
+    constructor(address owner) ERC20("Test Tether", "tUSDT") {
+    	_mint(owner, 1000000000000000000000);
     }
 }
--- erc20/npm-prj/scripts/deploy.js
+++ erc20/npm-prj/scripts/deploy.js
@@ -1,9 +1,8 @@
 async function main() {
   // We get the contract to deploy
     const USDT = await ethers.getContractFactory("USDT");
-    // TODO: Add here the exchange address which should
-    // have all the tUSDT
-    const usdt = await USDT.deploy("0x190FD61ED8fE0067f0f09EA992C1BF96209bab66", 1000000000000000);
+    const usdt = await USDT.deploy("0x190FD61ED8fE0067f0f09EA992C1BF96209bab66");
+
 
   console.log("USDT contract deployed to:", usdt.address);
 }
