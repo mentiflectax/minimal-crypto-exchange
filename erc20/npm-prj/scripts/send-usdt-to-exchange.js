f1ec9cfa7239cbdbcdd044c6bed99b9385e1cf89
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-15 20:43:38 +0100

    I-20: Attempt at bugfix.

--- erc20/npm-prj/scripts/send-usdt-to-exchange.js
+++ erc20/npm-prj/scripts/send-usdt-to-exchange.js
@@ -4,15 +4,15 @@ async function main() {
   "0xB816192c15160a2C1B4D032CDd7B1009583b21AF"
     );
     const amount = 1;
-    const gasLimit = 1000000;
+//    const gasLimit = 1000000;
     const exchange = "0x190FD61ED8fE0067f0f09EA992C1BF96209bab66";
     const usdtSender = "0xDd1e8cC92AF9748193459ADDF910E1b96E88154D";
 
     console.log("Approving the transfer...");
-    await usdt.approve(usdtSender, amount);
+    await usdt.approve(usdtSender, amount+1);
     console.log("Done");
     console.log("Sending USDT...");
-    const result = await usdt.transferFrom(usdtSender, exchange, amount, { gasLimit: gasLimit });
+    const result = await usdt.transferFrom(usdtSender, exchange, amount, { gasLimit: 1000000 });
     console.log("Done, result=", result);
 }
 
