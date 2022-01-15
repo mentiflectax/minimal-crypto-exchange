df71d7ed8cac7920674554dc60b6e442071be661
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-15 15:37:42 +0100

    I-20: Creating a script for creating the USDT sender account.

---
+++ eth/create-account-usdt-sender.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8178 \
+    -H "Content-Type: application/json" \
+   --data '{"jsonrpc":"2.0", "method":"personal_newAccount", "params":["only-mouse-in-a-mouse-trap-is-cheap"], "id":1235}'
