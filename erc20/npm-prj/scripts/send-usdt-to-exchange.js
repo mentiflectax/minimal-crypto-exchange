65ef5e0ce1b28109979f63bc5af0478b5f38d142
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-16 11:43:51 +0100

    I-20: Added the output of USDT sender's ETH balance.

--- erc20/npm-prj/scripts/send-usdt-to-exchange.js
+++ erc20/npm-prj/scripts/send-usdt-to-exchange.js
@@ -1,16 +1,21 @@
 async function main() {
     const USDT = await ethers.getContractFactory("USDT");
-    const usdt = await USDT.attach(
-  "0xB816192c15160a2C1B4D032CDd7B1009583b21AF"
-    );
+    const usdtAddress = "0xB816192c15160a2C1B4D032CDd7B1009583b21AF";
+    const usdt = await USDT.attach(usdtAddress);
     const amount = 1;
 //    const gasLimit = 1000000;
     const exchange = "0x190FD61ED8fE0067f0f09EA992C1BF96209bab66";
     const usdtSender = "0xDd1e8cC92AF9748193459ADDF910E1b96E88154D";
 
-    console.log("Approving the transfer...");
-    await usdt.approve(usdtSender, amount+1);
+    console.log("Determining the ETH balance of USDT sender...");
+    const ethBalance = await usdt.provider.getBalance(usdtAddress);
+    console.log("ETH balance:", ethBalance);
     console.log("Done");
+    
+    console.log("Approving the transfer...");
+    const approveResult = await usdt.approve(usdtSender, amount+1);
+    console.log("Done, result: ", approveResult);
+//    await usdt.allowance
     console.log("Sending USDT...");
     const result = await usdt.transferFrom(usdtSender, exchange, amount, { gasLimit: 1000000 });
     console.log("Done, result=", result);
