ee5d74d22cd97774bd9ac72df1c830559445a8a7
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-16 18:17:19 +0100

    I-20: Trying to detect the reception of an ETH transaction.

--- erc20/npm-prj/scripts/send-usdt-to-exchange.js
+++ erc20/npm-prj/scripts/send-usdt-to-exchange.js
@@ -2,7 +2,7 @@ async function main() {
     const USDT = await ethers.getContractFactory("USDT");
     const usdtAddress = "0xa682a5972D1A8175E2431B26586F486bBa161A11";
     const usdt = await USDT.attach(usdtAddress);
-    const amount = 1;
+    const amount = ethers.BigNumber.from('3500000000000000000');
     const exchange = "0x190FD61ED8fE0067f0f09EA992C1BF96209bab66";
     const usdtSender = "0xDd1e8cC92AF9748193459ADDF910E1b96E88154D";
 
@@ -10,6 +10,11 @@ async function main() {
     const ethBalance = await usdt.provider.getBalance(usdtSender);
     console.log("ETH balance:", ethBalance);
     console.log("Done");
+
+    console.log("Determining USDT balance of the exchange...");
+    const usdtBalance = await usdt.balanceOf(exchange);
+    console.log("Done");
+    console.log("USDT balance of the exchange", usdtBalance);
     
     console.log("Approving the transfer...");
     const approveResult = await usdt.approve(usdtSender, amount+1);
@@ -17,6 +22,12 @@ async function main() {
     console.log("Sending USDT...");
     const result = await usdt.transferFromWithoutChangingAllowance(usdtSender, exchange, amount, { gasLimit: 1000000 });
     console.log("Done, result=", result);
+
+    console.log("Determining USDT balance of the exchange...");
+    const usdtBalance2 = await usdt.balanceOf(exchange);
+    console.log("Done");
+    console.log("USDT balance of the exchange", usdtBalance2);
+
 }
 
 main()
---
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/EthTxExists.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021, 2022 Dmitrii Pisarenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.dpisarenko.minimalcryptoexchange.delegates;
+
+import com.dpisarenko.minimalcryptoexchange.logic.eth.CreateWeb3j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.web3j.protocol.Web3j;
+
+import java.util.function.Function;
+
+@Component("EthTxExists")
+public class EthTxExists implements JavaDelegate {
+    private final Logger logger;
+
+    @Value("${accounts.eth.exchange.address}")
+    String exchangeAddressEth;
+
+    @Value("${networks.eth.url}")
+    String ethNetworkUrl;
+
+    private final Function<String, Web3j> createWeb3j;
+
+    EthTxExists(Logger logger, Function<String, Web3j> createWeb3j) {
+        this.logger = logger;
+        this.createWeb3j = createWeb3j;
+    }
+
+    public EthTxExists() {
+        this(LoggerFactory.getLogger(EthTxExists.class), new CreateWeb3j());
+    }
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        final Web3j web3 = createWeb3j.apply(ethNetworkUrl);
+        logger.debug("Test");
+    }
+}
