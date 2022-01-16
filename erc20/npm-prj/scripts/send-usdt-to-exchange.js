8450d3035d7f004a3caf65c53172cc573088bd67
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-16 17:34:11 +0100

    I-20: Fixed the issue with sending USDT to the exchange.

--- erc20/npm-prj/scripts/send-usdt-to-exchange.js
+++ erc20/npm-prj/scripts/send-usdt-to-exchange.js
@@ -3,7 +3,6 @@ async function main() {
     const usdtAddress = "0xa682a5972D1A8175E2431B26586F486bBa161A11";
     const usdt = await USDT.attach(usdtAddress);
     const amount = 1;
-//    const gasLimit = 1000000;
     const exchange = "0x190FD61ED8fE0067f0f09EA992C1BF96209bab66";
     const usdtSender = "0xDd1e8cC92AF9748193459ADDF910E1b96E88154D";
 
@@ -15,7 +14,6 @@ async function main() {
     console.log("Approving the transfer...");
     const approveResult = await usdt.approve(usdtSender, amount+1);
     console.log("Done, result: ", approveResult);
-//    await usdt.allowance
     console.log("Sending USDT...");
     const result = await usdt.transferFromWithoutChangingAllowance(usdtSender, exchange, amount, { gasLimit: 1000000 });
     console.log("Done, result=", result);
