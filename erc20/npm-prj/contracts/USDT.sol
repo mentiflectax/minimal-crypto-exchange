2b9f1829d5737cecb599fea98e73d01d96634fe2
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-16 17:22:41 +0100

    I-20: Added transferFromWithoutChangingAllowance.

--- erc20/npm-prj/contracts/USDT.sol
+++ erc20/npm-prj/contracts/USDT.sol
@@ -7,4 +7,17 @@ contract USDT is ERC20 {
     	_mint(exchange, 1000000000000000000000);
     	_mint(usdtSender, 1000000000000000000000);
     }
+
+    function getAllowance(address src, address target) returns (uint256) {
+    	return _allowances[sender][target];
+    }
+
+    function transferFromWithoutChangingAllowance(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        return true;
+    }
 }
