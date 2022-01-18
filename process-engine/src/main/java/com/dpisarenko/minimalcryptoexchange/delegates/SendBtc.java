753a1aaf0952db3a5059fa1917fb512ab006d215
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-18 19:33:46 +0100

    I-20: Implementing sending BTC to the recipient.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/SendBtc.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/delegates/SendBtc.java
@@ -15,19 +15,10 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
-import java.math.BigInteger;
-
-@Component("ConvertUsdToUsdt")
-public class ConvertUsdToUsdt implements JavaDelegate {
-
-    public static final long USD_TO_USDT_CONVERSION_FACTOR = 1000000000000000000L;
-
+@Component("SendBtc")
+public class SendBtc implements JavaDelegate {
     @Override
     public void execute(final DelegateExecution delEx) throws Exception {
-        final Double usdAmount = (Double) delEx.getVariable("USD_AMOUNT");
-        final long usdtAmountLong = (long) (usdAmount * USD_TO_USDT_CONVERSION_FACTOR);
-        final BigInteger usdtAmount = BigInteger.valueOf(usdtAmountLong);
-        delEx.setVariable("USDT_AMOUNT", usdtAmount);
 
     }
 }
