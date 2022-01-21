d528c2b9c3561ad3199f4d933b503c07c5d737ad
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-21 22:01:08 +0100

    I-20: Writing and fixing tests.

--- process-engine/src/test/java/com/dpisarenko/minimalcryptoexchange/clojuredelegates/get_received_satoshis_Test.java
+++ process-engine/src/test/java/com/dpisarenko/minimalcryptoexchange/clojuredelegates/get_received_satoshis_Test.java
@@ -11,15 +11,14 @@
 
 package com.dpisarenko.minimalcryptoexchange.clojuredelegates;
 
-import clojure.java.api.Clojure;
 import com.dpisarenko.minimalcryptoexchange.clj.ClojureService;
 import org.bitcoinj.core.Coin;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.junit.Test;
 import org.slf4j.Logger;
 
-import static com.dpisarenko.minimalcryptoexchange.clj.ClojureService.MAIN_CLOJURE_NAMESPACE;
 import static com.dpisarenko.minimalcryptoexchange.clojuredelegates.TestUtils.createClojureBackend;
+import static com.dpisarenko.minimalcryptoexchange.clojuredelegates.TestUtils.initClojureBackend;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -27,13 +26,10 @@ import static org.mockito.Mockito.when;
 public class get_received_satoshis_Test {
     @Test
     public void givenDelegateExecution_whenExecute_thenSetReceivedSatoshisVariable() {
-        // TODO: Fix this
         // Given
         final Logger logger = mock(Logger.class);
         final ClojureService backend = createClojureBackend();
-
-        Clojure.var(MAIN_CLOJURE_NAMESPACE, "init")
-                .invoke(logger);
+        initClojureBackend(logger);
 
         backend.btcTxReceived("txId", Coin.valueOf(1000L));
 
