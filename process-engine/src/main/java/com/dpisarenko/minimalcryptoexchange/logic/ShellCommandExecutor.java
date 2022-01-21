fce07eabcb0f5caf34d0e2e0a564311f4717307f
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-21 18:57:32 +0100

    I-20: Implementing sending BTC to the recipient. Implementing the command-line option.

--- process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/ShellCommandExecutor.java
+++ process-engine/src/main/java/com/dpisarenko/minimalcryptoexchange/logic/ShellCommandExecutor.java
@@ -20,7 +20,6 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.Executors;
 
 public class ShellCommandExecutor {
     private final Logger logger;
