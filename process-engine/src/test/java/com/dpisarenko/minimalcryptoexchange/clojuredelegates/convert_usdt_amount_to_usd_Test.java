489b201e4bbb639fc8eb2207b69d1813e3b3d7b6
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-22 19:25:57 +0100

    I-20: Writing and fixing tests.

---
+++ process-engine/src/test/java/com/dpisarenko/minimalcryptoexchange/clojuredelegates/convert_usdt_amount_to_usd_Test.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021, 2022 Dmitrii Pisarenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.dpisarenko.minimalcryptoexchange.clojuredelegates;
+
+import com.dpisarenko.minimalcryptoexchange.clj.ClojureService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.math.BigDecimal;
+
+import static com.dpisarenko.minimalcryptoexchange.clojuredelegates.TestUtils.createClojureBackend;
+import static com.dpisarenko.minimalcryptoexchange.clojuredelegates.TestUtils.initClojureBackend;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class convert_usdt_amount_to_usd_Test {
+    @Test
+    public void givenUsdtAmount_whenExecute_thenSetUsdAmount() {
+        // Given
+        final Logger logger = mock(Logger.class);
+        final ClojureService backend = createClojureBackend();
+        initClojureBackend(logger);
+
+        final DelegateExecution delEx = mock(DelegateExecution.class);
+        delEx.setVariable("USDT_RECEIVED", BigDecimal.ONE);
+
+        // When
+        backend.runClojureCode(delEx, "convert-usdt-amount-to-usd");
+
+        // Then
+        verify(delEx).setVariable("USD_AMOUNT", BigDecimal.ZERO);
+    }
+}
